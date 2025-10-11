3f4f4c421ee328657ff5e6ee0e80dd5a1daf4877
UjjwalKumar7208 <ujjwal720946@gmail.com>
2025-10-11 08:49:54 +0530

    Auth middleware and little bit auth thing

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,45 @@
 const express = require('express')
+const zod = require('zod');
+const { User } = require( '../db' );
+const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = require( '../config' );
 const router = express.Router;
 
+const signupSchema = zod.object({
+    username: zod.string(),
+    password: zod.string(),
+    firstName: zod.string(),
+    lastName: zod.string()
+})
 
+router.post("/signup", async (req, res) => {
+    const body = req.body;
+    const {success} = signupSchema.safeParse(req.body)
+    if (!success) {
+        return  res.json({
+            message: "Email already taken / Incorrect inputs"
+        })
+    }
+
+    const user = User.findOne({
+        username: body.username
+    })
+
+    if (user._id) {
+        return  res.json({
+            message: "Email already taken / Incorrect inputs"
+        })
+    }
+
+    const dbUser = await User.create(body)
+    const token = user.sign({
+        userID: dbUser._id
+    }, JWT_SECRET)
+    res.json({
+        message: "User successfully created",
+        token: 
+    })
+})
 
 module.exports = router;
