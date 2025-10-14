88d6ffcee61615b3c17db930736dc4d9135073d9
UjjwalKumar7208 <ujjwal720946@gmail.com>
2025-10-14 08:23:09 +0530

    Frontend signup, dashboard and send amount login

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,14 @@
+import Heading from "../components/Heading";
+import Appbar from "../components/Appbar";
+import { Users } from "../components/Users";
+
 export default function Dashboard() {
     return (
-        <>
-
-        </>
+        <div>
+            <Appbar />
+            <div className="m-8">
+                <Users />
+            </div>
+        </div>
     )
 }
